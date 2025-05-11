e42bf664824f7cb2fb612763f00eed7871f6dca2
Filipe Santos <filipesantos1999@hotmail.com>
2025-05-11 18:17:22 +0100

    Change colors for SuperMaven suggestion

--- config/ide/neovim/nvim/lua/user/plugins/ai.lua
+++ config/ide/neovim/nvim/lua/user/plugins/ai.lua
@@ -4,15 +4,15 @@ return {
     config = function()
       require("supermaven-nvim").setup {
         keymaps = {
-          accept_suggestion = "<Tab>",
-          clear_suggestion = "<C-]>",
-          accept_word = "<C-j>",
+            accept_suggestion = "<C-l>",
+			accept_word = "<C-S-l>",
+			clear_suggestion = "<C-h>",
         },
         color = {
-          suggestion_color = "#d8d8d8",
+          suggestion_color = "#808080",
           cterm = 244,
         },
-        log_level = "info", -- set to "off" to disable logging completely
+        log_level = "off", -- set to "off" to disable logging completely
         disable_inline_completion = false, -- disables inline completion for use with cmp
         disable_keymaps = false, -- disables built in keymaps for more manual control
         condition = function() return false end,
